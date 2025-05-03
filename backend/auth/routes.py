e19480b0d55ee7c0d65dd5570b319f9d7b51c012
Aalify-X <dev.aalifyx@gmail.com>
2025-05-03 15:11:47 +0300

    Delete backend/auth directory

--- backend/auth/routes.py
+++
@@ -1,7 +0,0 @@
-import os
-import requests
-
-def is_valid_whop_user(token):
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get('https://api.whop.com/v1/me', headers=headers)
-    return response.status_code == 200
