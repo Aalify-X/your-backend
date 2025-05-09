0b37e03c964cdce06d2299b44b0d11fb63ad23af
Aalify-X <dev.aalifyx@gmail.com>
2025-05-09 08:59:58 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -55,7 +55,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not session.get('user'):
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('auth_bp.login'))
         return f(*args, **kwargs)
     return decorated_function
 
